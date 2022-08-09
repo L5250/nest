d8342a36d5823e73e2ad8538c83af563c093547a
L5250 <915016964@qq.com>
2022-08-09 17:39:41 +0800

    qnupload

--- src/modules/qnuploads/qnuploads.service.ts
+++ src/modules/qnuploads/qnuploads.service.ts
@@ -3,7 +3,7 @@
  * @Description: desc
  * @Date: 2022-08-08 16:10:08
  * @LastEditors: L5250
- * @LastEditTime: 2022-08-09 15:43:17
+ * @LastEditTime: 2022-08-09 17:39:26
  */
 import {
   HttpException,
@@ -13,7 +13,7 @@ import {
 } from '@nestjs/common';
 import { CreateQnUploadDto } from './dto/create-qnupload.dto';
 import { UpdateQnUploadDto } from './dto/update-qnupload.dto';
-import * as qn from 'qn';
+// import * as qn from 'qn';
 import * as qiniu from 'qiniu';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -25,7 +25,7 @@ export class QnUploadsService {
    * 官方SDK
    * 上传文件到七牛云
    */
-  async qiniuUpload(file: Express.Multer.File) {
+  async qiniuUpload(data, file: Express.Multer.File) {
     const mac = new qiniu.auth.digest.Mac(
       process.env.QINIU_ACCESSKEY,
       process.env.QINIU_SECRETKEY,
@@ -47,7 +47,7 @@ export class QnUploadsService {
         `${Date.now()}-${file.originalname}`,
         file.buffer,
         new qiniu.form_up.PutExtra(),
-        function (respErr, respBody, respInfo) {
+        (respErr, respBody, respInfo) => {
           if (respErr) {
             // console.error(respErr);
             throw new InternalServerErrorException(respErr.message);
@@ -58,6 +58,10 @@ export class QnUploadsService {
               // url: new url.URL(respBody.key, process.env.QINIU_HOST).href,
               url: `http://${process.env.QINIU_HOST}/${respBody.key}`,
             });
+            this.updateUser(
+              data,
+              `http://${process.env.QINIU_HOST}/${respBody.key}`,
+            );
           } else {
             // console.error(respInfo.statusCode, respBody);
             throw new InternalServerErrorException(respInfo);
@@ -67,75 +71,75 @@ export class QnUploadsService {
     });
   }
 
-  // 社区SDK
-  async qnUpload(data: CreateQnUploadDto, file: Express.Multer.File) {
-    // const fileName = `${data.id}/avatar/avatar.jpeg?x=${new Date().getTime()}`;
-    const fileName = `${data.id}/avatar`;
-    const client = qn.create({
-      // ak
-      accessKey: process.env.QINIU_ACCESSKEY,
-      // sk
-      secretKey: process.env.QINIU_SECRETKEY,
-      // scope 一般指文件要上传到的目标存储空间（Bucket）。
-      // 若为”Bucket”，表示限定只能传到该Bucket（仅限于新增文件）；若为”Bucket:Key”，表示限定特定的文件，可修改该文件。
-      // 仓库
-      // 只可以新增，重复会报错
-      bucket: process.env.QINIU_BUCKET,
-      /**
-       * 覆盖上传后更新
-       */
-      // bucket: process.env.QINIU_BUCKET + ':' + fileName,
-      // host
-      origin: process.env.QINIU_HOST,
-      uploadURL: 'http://up-z2.qiniup.com/',
-      // timeout: 3600000, // default rpc timeout: one hour, optional
-    });
-    client.list(fileName, (error, res) => {
-      console.log(res);
-      if (res.items && res.items.length > 0) {
-        console.log(res);
-        // return res;
-        // 删除上一张的图片
-        client.delete(res.items[0].key, (e) => {
-          // 新增一张
-          uu();
-        });
-      } else {
-        uu();
-      }
-    });
-    const uu = () => {
-      client.upload(
-        file.buffer,
-        {
-          // key: fileName,
-          key: `${fileName}/${new Date().getTime()}.jpeg`,
-        },
-        async (err, result) => {
-          console.log(result, 'err');
-          if (err) {
-            throw new HttpException(result.error, HttpStatus.FORBIDDEN);
-          } else {
-            // 更新user数据
-            console.log(result.url);
-            await this.updateUser(data, result);
-          }
-        },
-      );
-    };
-  }
+  // // 社区SDK
+  // async qnUpload(data: CreateQnUploadDto, file: Express.Multer.File) {
+  //   // const fileName = `${data.id}/avatar/avatar.jpeg?x=${new Date().getTime()}`;
+  //   const fileName = `${data.id}/avatar`;
+  //   const client = qn.create({
+  //     // ak
+  //     accessKey: process.env.QINIU_ACCESSKEY,
+  //     // sk
+  //     secretKey: process.env.QINIU_SECRETKEY,
+  //     // scope 一般指文件要上传到的目标存储空间（Bucket）。
+  //     // 若为”Bucket”，表示限定只能传到该Bucket（仅限于新增文件）；若为”Bucket:Key”，表示限定特定的文件，可修改该文件。
+  //     // 仓库
+  //     // 只可以新增，重复会报错
+  //     bucket: process.env.QINIU_SCOPE,
+  //     /**
+  //      * 覆盖上传后更新
+  //      */
+  //     // bucket: process.env.QINIU_SCOPE + ':' + fileName,
+  //     // host
+  //     origin: process.env.QINIU_HOST,
+  //     uploadURL: 'http://up-z2.qiniup.com/',
+  //     // timeout: 3600000, // default rpc timeout: one hour, optional
+  //   });
+  //   client.list(fileName, (error, res) => {
+  //     console.log(res);
+  //     if (res.items && res.items.length > 0) {
+  //       console.log(res);
+  //       // return res;
+  //       // 删除上一张的图片
+  //       client.delete(res.items[0].key, (e) => {
+  //         // 新增一张
+  //         uu();
+  //       });
+  //     } else {
+  //       uu();
+  //     }
+  //   });
+  //   const uu = () => {
+  //     client.upload(
+  //       file.buffer,
+  //       {
+  //         // key: fileName,
+  //         key: `${fileName}/${new Date().getTime()}.jpeg`,
+  //       },
+  //       async (err, result) => {
+  //         console.log(result, 'err');
+  //         if (err) {
+  //           throw new HttpException(result.error, HttpStatus.FORBIDDEN);
+  //         } else {
+  //           // 更新user数据
+  //           console.log(result.url);
+  //           await this.updateUser(data, `http://${result.url}`);
+  //         }
+  //       },
+  //     );
+  //   };
+  // }
 
-  async updateUser(data: CreateQnUploadDto, result: { url: any }) {
+  async updateUser(data: CreateQnUploadDto, url: any) {
     await this.prisma.blogUser.update({
       where: { id: data.id },
-      data: { avatarUrl: `http://${result.url}` },
+      data: { avatarUrl: url },
     });
     return true;
   }
 
   // 上传头像
   async qiniuUploadAvatar(data: CreateQnUploadDto, file: Express.Multer.File) {
-    // const a: any = await this.qiniuUpload(file);
-    this.qnUpload(data, file);
+    const a: any = await this.qiniuUpload(data, file);
+    // this.qnUpload(data, file);
   }
 }
